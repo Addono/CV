c5113b43b02d6f96602917441963a02cd329f04a
Adriaan Knapen <hi@aknapen.nl>
2019-12-10 13:15:40 +0100

    :bug: Open PDF in a new tab to prevent filetype collision

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,40 +1,48 @@
 import React from 'react';
 import Head from "next/head";
 
-import {Button, Card, CardActions, CardContent, Container, Typography} from "@material-ui/core";
+import { Button, Card, CardActions, CardContent, Container, Typography } from "@material-ui/core";
 import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
-const pdfLocation = "//cv.aknapen.nl/cv.pdf";
+const pdfLocation = "https://cv.aknapen.nl/cv.pdf";
 
-const Home = () => {
-    return <object data={pdfLocation} type="application/pdf" style={{float: 'left', width: '100%', height: '100vh'}}>
-        <Container style={{marginTop: '1em'}}>
-            <Card>
-                <CardContent>
-                    <Typography variant={"h4"} component={"h4"}>
-                        CV Adriaan Knapen
-                    </Typography>
-                    <Typography variant="body2" component={"p"}>
-                        Probably you're on a mobile device, so you will have to tell me how you want to display my CV.
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Container>
-                    <Button size="large" href={pdfLocation}>As PDF <PictureAsPdfIcon/></Button>
-                    <Button size={"large"} href={"https://linkedin.com/in/adriaan-knapen"}>On LinkedIn <LinkedInIcon/></Button>
-                    </Container>
-                </CardActions>
-            </Card>
-        </Container>
-    </object>;
-};
+const Home: React.FC = () => (
+    <object data={pdfLocation} type="application/pdf" style={{ float: 'left', width: '100%', height: '100vh' }}>
+        <ResumeFormatSelector />
+    </object>
+)
+
+const ResumeFormatSelector: React.FC = () => (
+    <Container style={{ marginTop: '1em' }}>
+        <Card>
+            <CardContent>
+                <Typography variant={"h4"} component={"h4"}>
+                    CV Adriaan Knapen
+                </Typography>
+                <Typography variant="body2" component={"p"}>
+                    Probably you're on a mobile device, so you will have to tell me how you want to display my CV.
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Container>
+                    <a href={pdfLocation} target="_blank" rel="noopener noreferrer">
+                        <Button size={"large"}>As PDF <PictureAsPdfIcon /></Button>
+                    </a>
+                    <a href={"https://linkedin.com/in/adriaan-knapen"}>
+                        <Button size={"large"}>On LinkedIn <LinkedInIcon /></Button>
+                    </a>
+                </Container>
+            </CardActions>
+        </Card>
+    </Container>
+)
 
 export default () => (
-    <div>
+    <>
         <Head>
             <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
-            <meta charSet="utf-8"/>
+            <meta charSet="utf-8" />
             <title>
                 CV - Adriaan Knapen
             </title>
@@ -45,6 +53,6 @@ export default () => (
       }
     `}</style>
         <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <Home/>
-    </div>
+        <Home />
+    </>
 )
