967c289cc8208b8668705bb49f4d6da9be7cd60f
Adriaan Knapen <hi@aknapen.nl>
2024-12-10 09:39:48 +0100

    chore(website): update all dependencies to the latest version

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -8,9 +8,9 @@ import {
   CardContent,
   Container,
   Typography,
-} from "@material-ui/core";
-import PictureAsPdfIcon from "@material-ui/icons/PictureAsPdf";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
+} from "@mui/material";
+import ArticleIcon from '@mui/icons-material/Article';
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const pdfUrl = "https://cv.aknapen.nl/cv.pdf";
 const linkedinUrl = "https://linkedin.com/in/adriaan-knapen";
@@ -19,7 +19,8 @@ const Home: React.FC = () => {
   const [isMobile, setIsMobile] = React.useState(false);
 
   React.useEffect(() => {
-    const userAgent = typeof window.navigator === "undefined" ? "" : navigator.userAgent;
+    const userAgent =
+      typeof window.navigator === "undefined" ? "" : navigator.userAgent;
     const mobile = Boolean(
       userAgent.match(
         /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
@@ -49,20 +50,43 @@ const ResumeFormatSelector: React.FC = () => (
   <Container style={{ marginTop: "2em", textAlign: "center" }}>
     <Card>
       <CardContent>
-        <Typography variant="body1" component={"p"} style={{ marginTop: "1em" }}>
-          It looks like you're using a mobile device. For the best viewing experience, you can download my CV as a PDF or view my LinkedIn page. Choose an option below:
+        <Typography
+          variant="body1"
+          component={"p"}
+          style={{ marginTop: "1em" }}
+        >
+          It looks like you're using a mobile device. For the best viewing
+          experience, you can download my CV as a PDF or view my LinkedIn page.
+          Choose an option below:
         </Typography>
       </CardContent>
       <CardActions style={{ justifyContent: "center" }}>
         <Container>
-          <a href={pdfUrl} target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", marginBottom: "10px" }}>
-            <Button variant="contained" color="primary" size={"large"} startIcon={<PictureAsPdfIcon />} style={{ margin: ".5em"}}>
-              Download PDF
+          <a
+            href={pdfUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none", marginBottom: "10px" }}
+          >
+            <Button
+              variant="contained"
+              color={"primary"}
+              size={"large"}
+              startIcon={<ArticleIcon />}
+              style={{ margin: ".5em" }}
+            >
+              Open As PDF
             </Button>
           </a>
           <a href={linkedinUrl} style={{ textDecoration: "none" }}>
-            <Button variant="contained" color="primary" size={"large"} startIcon={<LinkedInIcon />} style={{ margin: ".5em"}}>
-              View on LinkedIn
+            <Button
+              variant="contained"
+              color="primary"
+              size={"large"}
+              startIcon={<LinkedInIcon />}
+              style={{ margin: ".5em" }}
+            >
+              View LinkedIn
             </Button>
           </a>
         </Container>
@@ -84,7 +108,7 @@ export default () => (
     <style jsx global>{`
       body {
         margin: 0;
-        font-family: 'Roboto', sans-serif;
+        font-family: "Roboto", sans-serif;
         background-color: #f5f5f5;
       }
     `}</style>
@@ -92,19 +116,21 @@ export default () => (
       rel="stylesheet"
       href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
     />
-    <div style={{ 
-      backgroundColor: '#3f51b5', 
-      color: 'white', 
-      padding: '1em .5em', 
-      textAlign: 'center' 
-    }}>
+    <Container
+      sx={{
+        backgroundColor: "primary.main",
+        color: "white",
+        padding: "1em .5em",
+        textAlign: "center",
+      }}
+    >
       <Typography variant="h3" component="h1" style={{ margin: 0 }}>
         Resume <b>Adriaan&nbsp;Knapen</b>
       </Typography>
-      <Typography variant="h6" component="p" style={{ marginTop: '0.5em' }}>
+      <Typography variant="h6" component="p" style={{ marginTop: "0.5em" }}>
         Welcome to my professional CV
       </Typography>
-    </div>
+    </Container>
     <Home />
   </>
 );
